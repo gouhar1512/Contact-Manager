58093e5abb1fe2942b5fc389f3054a0741e706b6
gouhar1512 <ushran987@gmail.com>
2020-07-25 00:21:40 +0530

    Updates

---
+++ precache-manifest.59b677729e17d5d6aeb044b237cb41ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c4f51818f1c7526c02db9d9ccd9f68b",
+    "url": "/contact-manager/index.html"
+  },
+  {
+    "revision": "fe0cba4ac9da974ba199",
+    "url": "/contact-manager/static/css/main.1301d454.chunk.css"
+  },
+  {
+    "revision": "257d8b79c9c9efefd5c5",
+    "url": "/contact-manager/static/js/2.f97bf880.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/contact-manager/static/js/2.f97bf880.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe0cba4ac9da974ba199",
+    "url": "/contact-manager/static/js/main.e73c2c8d.chunk.js"
+  },
+  {
+    "revision": "24c1a1cc9acd61c4cfb9",
+    "url": "/contact-manager/static/js/runtime-main.9e8a2e75.js"
+  }
+]);
